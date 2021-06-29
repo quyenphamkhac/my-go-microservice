c2db3a0855e4c2043e1596ac273eb1eebb422a14
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-06-29 18:16:15 +0700

    feat: my first sample with grpc

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/quyenphamkhac/skoppi/handler"
+	pb "github.com/quyenphamkhac/skoppi/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &handler.HelloSrv{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
